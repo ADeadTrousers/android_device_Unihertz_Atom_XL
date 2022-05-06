98adb59cb72616417fdca33594615e510cc3e064
Matthias Leitl <a.dead.trousers@gmail.com>
2022-05-06 14:00:41 +0200

    Obviously no more special sepolicies for the XL

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -10,13 +10,5 @@ DEVICE_MODEL_PATH := device/Unihertz/Atom_XL
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_MODEL_PATH)/bluetooth/include
 
-# Sepolicy
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    $(DEVICE_MODEL_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(DEVICE_MODEL_PATH)/sepolicy/private
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(DEVICE_MODEL_PATH)/sepolicy/vendor
-
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_MODEL_PATH)/product.prop
